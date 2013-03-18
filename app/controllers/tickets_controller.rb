8e331f513bf161adee232b312e68f94efb79fa06
Yuzhe <yuzhe.c85@gmail.com>
2013-03-18 18:59:41 +0800

    套後台前

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -16,14 +16,15 @@ class TicketsController < ApplicationController
   # POST /tickets
   # POST /tickets.json
   def create
-    @ticket = Ticket.new(params[:ticket])
-
+  
+    @ticket = Ticket.new( params[:ticket] )
+     
     respond_to do |format|
       if @ticket.save
       
         TicketMailer.send_notice(@ticket).deliver
       
-        format.html { redirect_to :action => :new, notice: 'Ticket was successfully created.' }
+        format.html { redirect_to :action => :new, :anchor => "finished" }
         format.json { render json: @ticket, status: :created, location: @ticket }
       else
         format.html { render action: "new" }
--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -6,7 +6,7 @@ class TicketMailer < ActionMailer::Base
   	@ticket = ticket
 
   	mail(:to => ["yuzhe@summers.com.tw"],
-  	     :subject => "[夏天廣告官網] 收到使用者詢問囉！：#{@ticket.name}")
+  	     :subject => "[夏天廣告官網] 收到使用者詢問囉！")
   	
   end
 end
